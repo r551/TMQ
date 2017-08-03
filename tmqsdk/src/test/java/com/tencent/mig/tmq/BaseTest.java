14b239bcf1e5a388355dbf3d933c4696b47f1d3d
yoyoqin <18660226@qq.com>
2017-08-03 11:52:53 +0800

    更新抽象测试类的环境清理代码，修改部分用例不通过的问题

--- tmqsdk/src/test/java/com/tencent/mig/tmq/BaseTest.java
+++ tmqsdk/src/test/java/com/tencent/mig/tmq/BaseTest.java
@@ -1,9 +1,15 @@
 package com.tencent.mig.tmq;
 
+import com.tencent.mig.tmq.simple.ModeEnum;
+import com.tencent.mig.tmq.simple.SimpleTMQ;
+import com.tencent.mig.tmq.weak.WeakTMQ;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
+
+import java.util.ArrayList;
 
 /**
  * 测试父类
@@ -19,6 +25,10 @@ public class BaseTest {
     @Before
     public void setUp()
     {
+        TMQ.init(new SimpleTMQ());
+        TMQ.reset(); // 注意用例里的其他线程要及时关闭，否则连续执行可能会影响下一个用例的结果
+        TMQ.switchExpectMode(ModeEnum.STRICT);
+
         TMQ.setOutStream(System.out);
         TMQ.printText(testName.getMethodName() + " start..");
     }
@@ -26,6 +36,8 @@ public class BaseTest {
     @After
     public void tearDown()
     {
+        TMQ.setCheckListener(null);
+
         TMQ.setOutStream(System.out);
         TMQ.printText(testName.getMethodName() + " end.");
     }
